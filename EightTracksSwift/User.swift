5a990fa363b285a1dd85bc5cec80d3114967f034
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-06-29 14:25:27 +0300

    user model

--- EightTracksSwift/User.swift
+++ EightTracksSwift/User.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class User: NSObject {
-   
+    let user_token:String?
+    
+    init() {
+        
+    }
+    
+    func isLogined() -> Bool {
+        let storage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        let cookies = NSHTTPCookieStorage.cookiesForURL(storage)(NSURL.URLWithString(API_URL))
+        
+        if (cookies.count > 0) {
+            return true
+        }
+        
+        return false
+    }
+    
+    func logout() {
+        let storage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        let cookies = NSHTTPCookieStorage.cookiesForURL(storage)(NSURL.URLWithString(API_URL))
+        
+        for cookie:AnyObject in cookies {
+            storage.deleteCookie(cookie as NSHTTPCookie)
+        }
+    }
 }
--- EightTracksSwift/WelcomeViewController.swift
+++ EightTracksSwift/WelcomeViewController.swift
@@ -22,6 +22,10 @@ class WelcomeViewController: UIViewController, UIViewControllerTransitioningDele
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let user = User()
+        if (user.isLogined()) {
+            println("segue to nav controller")
+        }
         // Do any additional setup after loading the view.
     }
 
